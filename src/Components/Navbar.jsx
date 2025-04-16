b03bb26f752ffda43efbb30084d095e27f6d0e98
suraj savle <surajsavle143@gmail.com>
2025-04-16 10:19:40 +0530

    Lets start

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+      <nav className="bg">
+          
+    </nav>
+  );
+}
+
+export default Navbar;
